e455c17f78ccd6575a83a9a43dede367cbe432ee
MatthewGadsden <matthewjgadsden@gmail.com>
2025-03-28 17:01:21 +1100

    feat: use chrome storage options

--- src/functions/setup.ts
+++ src/functions/setup.ts
@@ -1,14 +1,44 @@
-import { Links } from '../types/Link';
-
 export const getCurrentTab = async () => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   return tabs[0];
 };
 
-export const loadLinks = async () => {
-  const data = localStorage.getItem('links');
-  if (!data) return { links: [] };
+export const getStorageType = async () => {
+  var useSyncStorage: boolean = false;
 
-  const storedLinks: Links = JSON.parse(data);
-  return storedLinks;
+  await chrome.storage.sync.get('useSyncStorage').then((result) => {
+    useSyncStorage = result?.useSyncStorage ?? false;
+  });
+
+  return useSyncStorage ? chrome.storage.sync : chrome.storage.local;
+}
+
+export const loadLinks = async () => {
+  let myLinks = { links: [] };
+
+  const storage = await getStorageType();
+
+  await storage.get('json').then((result) => {
+    if (!result.json) return;
+
+    myLinks = JSON.parse(result.json);
+  });
+
+  return myLinks;
+};
+
+export interface Options {
+  useSyncStorage: boolean;
+}
+
+export const getCurrentOptions = async (): Promise<Options> => {
+  var useSyncStorage: boolean = false;
+
+  await chrome.storage.sync.get('useSyncStorage').then((result) => {
+    useSyncStorage = result?.useSyncStorage ?? false;
+  });
+
+  return {
+    useSyncStorage,
+  };
 };
