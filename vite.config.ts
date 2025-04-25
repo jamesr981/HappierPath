47882928b0b6fecfed3a25cd43f2d4abfa1aec1d
James Reynolds <jreynolds@compass.education>
2025-04-25 15:02:48 +0100

    Fixed formatting

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         input: {
           popup: 'popup.html',
-	        options: 'options.html',
+          options: 'options.html',
           background: 'src/background.ts'
         },
         output: {
