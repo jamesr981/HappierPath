9231ab3db092d43a66b488e09617bf511e77fe79
James Reynolds <jreynolds@compass.education>
2025-04-25 13:42:42 +0100

    Changes to test pipeline

--- src/options.tsx
+++ src/options.tsx
@@ -6,5 +6,5 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <OptionsApp />
-  </React.StrictMode>
+  </React.StrictMode>uuvhiovdhoisd
 );
