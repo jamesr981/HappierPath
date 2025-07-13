3fa97c3fbcfcf71692659adff1dfbb6525a9bfdc
James Reynolds <jreynolds@compass.education>
2025-07-13 16:29:47 +1000

    Converted OptionsApp and removed more css

--- src/OptionsApp.tsx
+++ src/OptionsApp.tsx
@@ -7,6 +7,7 @@ import {
   saveOptionsToStorage,
 } from './functions/storage';
 import { Options } from './types/Options';
+import { Box, Typography, Paper, Stack, Checkbox, FormControlLabel } from '@mui/material';
 
 const OptionsApp = () => {
   const [options, setOptions] = useState<Options>({ useSyncStorage: false });
@@ -28,34 +29,36 @@ const OptionsApp = () => {
   }, []);
 
   return (
-    <>
-      <div className="header">
-        <h3>Options</h3>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          verticalAlign: 'middle',
-          padding: '10px 10px 20px 5px',
-        }}
-      >
-        <h2>Sync Paths to Account:</h2>
-        <input
-          type="checkbox"
-          checked={options.useSyncStorage}
-          id="useSyncStorage"
-          style={{ verticalAlign: 'middle', marginLeft: 'auto' }}
-          onChange={() =>
-            changeOptions({
-              ...options,
-              useSyncStorage: !options.useSyncStorage,
-            })
-          }
-        />
-      </div>
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h5" sx={{ fontWeight: 700, mb: 2 }}>
+        Options
+      </Typography>
+      <Paper sx={{ p: 2, mb: 2 }} elevation={1}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+            Sync Paths to Account:
+          </Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={options.useSyncStorage}
+                id="useSyncStorage"
+                color="primary"
+                onChange={() =>
+                  changeOptions({
+                    ...options,
+                    useSyncStorage: !options.useSyncStorage,
+                  })
+                }
+              />
+            }
+            label=""
+            sx={{ ml: 'auto' }}
+          />
+        </Stack>
+      </Paper>
       <SupportButtons />
-    </>
+    </Box>
   );
 };
 
