2b4532afc051af8d2451e8cc06224461cb904aa9
James Reynolds <jreynolds@compass.education>
2025-07-14 20:51:09 +1000

    Fixed build issue

--- src/options.tsx
+++ src/options.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import OptionsApp from './OptionsApp';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider } from './contexts/ThemeProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
